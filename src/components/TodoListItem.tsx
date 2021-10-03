fedc05ce9d9a4ad90821fa1a4f14c50035cbc452
Ian Yong <ianyongyc@gmail.com>
2021-10-03 13:32:23 +0800

    Style todo list item

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { ListItem } from '@mui/material';
+import { Chip, Grid, Paper, Typography } from '@mui/material';
+import DateRangeIcon from '@mui/icons-material/DateRange';
+import moment from 'moment';
 import { TodoListData } from '../types/todos';
+
+const PaperStyle = {
+  margin: '20px',
+  padding: '20px',
+  backgroundColor: 'whitesmoke',
+};
 
 interface Props {
   todo: TodoListData;
 }
 
-const TodoListItem: React.FunctionComponent<Props> = ({ todo }: Props) => <ListItem>{todo.name}</ListItem>;
+const TodoListItem: React.FunctionComponent<Props> = ({ todo }: Props) => (
+  <Paper style={PaperStyle}>
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography variant="h2">{todo.name}</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Chip icon={<DateRangeIcon />} variant="outlined" label={`Due ${moment(todo.dueDate).format('D MMMM YYYY')}`} />
+      </Grid>
+    </Grid>
+  </Paper>
+);
 
 export default TodoListItem;
