2eddadbbcb5d61a28f35623c69ac4bdfbdfb1964
Ian Yong <ianyongyc@gmail.com>
2021-10-03 11:16:54 +0800

    Add todo store types

---
+++ src/store/todos/types.ts
@@ -0,0 +1,29 @@
+import { TodoData, TodoListData } from '../../types/todos';
+
+// Action names
+export const SAVE_TODOS_LIST = 'todos/SAVE_TODOS_LIST';
+export const SAVE_TODO = 'todos/SAVE_TODO';
+export const REMOVE_TODO = 'todos/REMOVE_TODO';
+
+// Action types
+export interface SaveTodosListAction {
+  type: typeof SAVE_TODOS_LIST;
+  todosList: TodoListData[];
+}
+
+export interface SaveTodoAction {
+  type: typeof SAVE_TODO;
+  todo: TodoData;
+}
+
+export interface RemoveTodoAction {
+  type: typeof REMOVE_TODO;
+  todoId: number;
+}
+
+export type TodoActionTypes = SaveTodosListAction | SaveTodoAction | RemoveTodoAction;
+
+export interface TodosState {
+  todoList: TodoListData[];
+  todos: Record<number, TodoData>;
+}
